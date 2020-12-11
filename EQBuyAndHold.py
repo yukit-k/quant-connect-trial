7a7fac8b1d0b649151f979b8af1d076d2e59c18d
yuki678 <uk.12juni@gmail.com>
2020-12-11 08:00:43 +0000

    Add EQBuyAndHold Class

---
+++ EQBuyAndHold.py
@@ -0,0 +1,17 @@
+class EQBuyHold(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2017, 6, 1)
+        self.SetEndDate(2017, 6, 15)
+
+        #1,2. Select IWM minute resolution data and set it to Raw normalization mode
+        self.iwm = self.AddEquity("IWM", Resolution.Minute)
+        self.iwm.SetDataNormalizationMode(DataNormalizationMode.Raw)
+
+    def OnData(self, data):
+
+        #3. Place an order for 100 shares of IWM and print the average fill price
+        #4. Debug the AveragePrice of IWM
+        if not self.Portfolio.Invested:
+            self.MarketOrder('IWM', 100)
+            self.Debug(str(self.Portfolio['IWM'].AveragePrice))
